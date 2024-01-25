446018a018aaaa3a586c46d30b765c415a3230c4
sanya09ta <86514082+sanya09ta@users.noreply.github.com>
2024-01-25 08:50:30 +0700

    Add files via upload

---
+++ spin_game/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sct Fair 2024</title>
+	<link rel="stylesheet" href="css/style.css?v=1.0001">
+</head>
+<body>
+	<div class="nav-top">
+		<button class="nav-button">
+			<span>คะแนน </span>
+			<span id="score"></span>
+		</button>
+		<button class="nav-button">
+			<span id="reward">-</span>
+		</button>
+	</div>
+	<div id="mainCanvas" class="container">
+		<div class="arrow">
+			<span>▼</span>
+		</div>
+		<div id="spin">
+			<div>
+			</div>
+			<div>
+				<img src="img/dear.png" draggable="false">
+			</div>
+			<div>
+			</div>
+			<div>
+				<img src="img/zee.png"  draggable="false">
+			</div>
+			<div>
+			</div>
+			<div>
+				<img src="img/dream.png"  draggable="false">
+			</div>
+		</div>
+	</div>
+	<script src="vendor/jquery/jquery-3.7.1.js"></script>
+	<script src="js/script.js?v=1.0001"></script>
+</body>
+</html>
